51cd2fdc8099a7eaa4722b9d5aa6439f43fa8ecd
xufutao1 <3265104480@qq.com>
2025-09-27 18:16:10 +0800

    Add files via upload

---
+++ 9.26-佛狸-第1题.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main() {
+    int school, extra, total;
+    int result = 0;
+    int _result = 0;
+    for (int i = 0; i < 7; i++) {
+        scanf_s("%d %d", &school, &extra);
+        total = school + extra;
+        if (total > 8) {
+            _result = total;
+            result = i;
+            if (total > _result)result = i;
+
+
+        }
+        
+    }
+    printf("%d", result);
+
+
+    return 0;
+}
